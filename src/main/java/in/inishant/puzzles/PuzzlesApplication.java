88f2189876ef675ab6c470be582f931e55a187ee
nltr <learning@inishant.in>
2018-01-28 16:17:27 +0530

    Initial Package structure

---
+++ src/main/java/in/inishant/puzzles/PuzzlesApplication.java
@@ -0,0 +1,12 @@
+package in.inishant.puzzles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzlesApplication.class, args);
+	}
+}
