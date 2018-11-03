6259322de073a9b6c394f0b59cd03e1a92b90870
nltr <learning@inishant.in>
2018-11-03 22:38:28 +0530

    LeetCode Puzzle

---
+++ src/main/java/in/inishant/puzzles/LeetCode727.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package in.inishant.puzzles;
+
+import java.util.Arrays;
+
+/**
+ * @author Nishant
+ *
+ */
+public class LeetCode727 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int nums[]= {0,0,1,1,1,2,2,3,3,4};
+		System.out.println(removeDuplicates(nums));
+		System.out.println(Arrays.toString(nums));
+	}
+
+	public static int removeDuplicates(int[] nums) {
+		int cp = 0,i = 0,count = 0;
+		while(cp<nums.length) {
+			if(cp == i) {
+				cp++;
+				count++;
+			}else if(nums[cp]==nums[i]) {
+				cp++;
+			}else {
+				i++;
+				nums[i]=nums[cp];
+				cp++;
+				count++;
+			}
+			
+		}
+		return count;
+	}
+
+}
